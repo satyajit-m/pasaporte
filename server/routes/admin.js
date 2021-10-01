a1525fc2edd587be40328c6841beede911d71b7c
smohapatra200 <smohapatra200@gmail.com>
2021-10-01 09:00:32 +0530

    backend admin handle update

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,15 +6,16 @@ router.get("/verified", async (req, res) => {
 
   try {
     const getVerified = await pool.query(
-      "SELECT *FROM applications A INNER JOIN verifications B ON A.appl_id=B.appl_id WHERE B.police_ver='approved' AND B.admin_ver=true"
+      "SELECT *FROM applications A INNER JOIN verifications B ON A.appl_id=B.appl_id WHERE B.police_ver='approved' AND B.admin_ver=approved"
     );
 
-    const response = getVerified.rows;
-    if (response.length != 0) {
-      res.json(response);
-    } else {
-      res.json("No Verified Records Found");
-    }
+    res.status(200).json({
+      status: "success",
+      results: getVerified.rows.length,
+      data: {
+        verifiedRecords: getVerified.rows,
+      },
+    });
   } catch (err) {
     console.error(err.message);
     return res.status(500).send("Server Error");
@@ -45,6 +46,27 @@ router.post("/approve", async (req, res) => {
     const { id } = req.body;
     const getVerified = await pool.query(
       "UPDATE verifications SET admin_ver='approved' WHERE appl_id = $1",
+      [id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: getVerified.rows.length,
+      data: {
+        verifiedRecords: getVerified.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
+
+router.post("/reject", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const getVerified = await pool.query(
+      "UPDATE verifications SET admin_ver='reject' WHERE appl_id = $1",
       [id]
     );
 
